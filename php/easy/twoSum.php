a068f6d1af52cee88a6d517ce77dc1a8b3a4608c
Elvocool <elvocool@gmail.com>
2023-07-05 13:04:15 +0300

    two sum question

--- php/easy/twoSum.php
+++ php/easy/twoSum.php
@@ -56,4 +56,15 @@ class Solution {
     }
 }
 
+/*
+// Test case
+
+$solution = new Solution();
+$nums = [2,7,11,15];
+$target = 9;
+$result = $solution->twoSum($nums, $target);
+print_r($result);
+
+*/
+
 ?>
